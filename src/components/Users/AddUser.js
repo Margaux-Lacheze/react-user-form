966fde5f9351320aa5b5e07a55032d2cfc6ae8e8
margauxlacheze <margaux.lchz@gmail.com>
2023-03-14 11:09:25 +0100

    gestion du state de AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
 
+  const usernameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  };
   // on peut dire className pour Card (alors que c'est notre propre composant) car on l'a passé comme prop dans Card
   // normalement className est utilisé pour les composants html build in de React (div, label etc)
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Identifiant</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" onChange={usernameChangeHandler}></input>
         <label htmlFor="age">Age (en années)</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
         <Button type="submit">Ajouter un utilisateur</Button>
       </form>
     </Card>
