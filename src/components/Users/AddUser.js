76da1c954f084810086c0491c53a138c5896df37
margauxlacheze <margaux.lchz@gmail.com>
2023-03-14 19:19:20 +0100

    ajout d'un composant UsersList

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,28 +8,28 @@ const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
-
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-        return;
+      return;
     }
 
     // le + permet de forcer la conversion du string en number
     if (+enteredAge < 1) {
-        return;
+      return;
     }
-    console.log(enteredUsername, enteredAge);
+    // pointeur vers onAddUser d'App.js
+    props.onAddUser(enteredUsername, enteredAge);
     // pour permettre de clear en changeant la value dans le form
-    setEnteredUsername('');
-    setEnteredAge('');
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
-  const usernameChangeHandler = event => {
+  const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
   };
 
-  const ageChangeHandler = event => {
+  const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
   // on peut dire className pour Card (alors que c'est notre propre composant) car on l'a passé comme prop dans Card
@@ -38,9 +38,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Identifiant</label>
-        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}></input>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (en années)</label>
-        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit">Ajouter un utilisateur</Button>
       </form>
     </Card>
