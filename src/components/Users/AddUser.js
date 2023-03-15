87f1677e75f2dfb4436e5350cd75f73d3ea9a16e
margauxlacheze <margaux.lchz@gmail.com>
2023-03-15 14:20:34 +0100

    ajout composant ErrorModal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -35,25 +36,31 @@ const AddUser = (props) => {
   // on peut dire className pour Card (alors que c'est notre propre composant) car on l'a passé comme prop dans Card
   // normalement className est utilisé pour les composants html build in de React (div, label etc)
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Identifiant</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        ></input>
-        <label htmlFor="age">Age (en années)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        ></input>
-        <Button type="submit">Ajouter un utilisateur</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal
+        title="Il y a eu une erreur !"
+        message="Houston, on a un problème !"
+      ></ErrorModal>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Identifiant</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlFor="age">Age (en années)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Ajouter un utilisateur</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
