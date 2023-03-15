d22c77453809e656361be7a7c1a8c0dc665392f0
margauxlacheze <margaux.lchz@gmail.com>
2023-03-15 14:34:53 +0100

    Gestion dynamique de la modale

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,15 +8,24 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Identifiant invalide",
+        message: "Veuillez entrer un identifiant valide."
+      });
       return;
     }
 
     // le + permet de forcer la conversion du string en number
     if (+enteredAge < 1) {
+      setError({
+        title: "Age invalide",
+        message: "Veuillez entrer un âge valide (>0)."
+      });
       return;
     }
     // pointeur vers onAddUser d'App.js
@@ -33,14 +42,20 @@ const AddUser = (props) => {
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
+
+  const errorHandler = () => {
+    setError(null);
+  }
   // on peut dire className pour Card (alors que c'est notre propre composant) car on l'a passé comme prop dans Card
   // normalement className est utilisé pour les composants html build in de React (div, label etc)
+  // retourner conditionnellement erreur
   return (
     <div>
-      <ErrorModal
-        title="Il y a eu une erreur !"
-        message="Houston, on a un problème !"
-      ></ErrorModal>
+      {error && <ErrorModal
+        title={error.title}
+        message={error.message}
+        onConfirm={errorHandler}
+      ></ErrorModal>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Identifiant</label>
